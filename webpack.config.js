9f0fb0760b545f6c02feeceb4168b9845ee3c13a
Israel <israel.facanha@gmail.com>
2020-06-26 11:49:05 -0300

    Add sass and css loaders to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,14 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /.s[ac]ss$/i,
+                use : [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
